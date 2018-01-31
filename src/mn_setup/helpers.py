557b8dad8e5244ec018d9f37f8dddc0308c91fae
Dan Lynch <pyramation@gmail.com>
2018-01-31 02:18:18 -0800

    init.id help

--- src/mn_setup/helpers.py
+++ src/mn_setup/helpers.py
@@ -39,11 +39,100 @@ DCONF='%s/denarius.conf' % DATADIR
 __escape_decoder = codecs.getdecoder('unicode_escape')
 __posix_variable = re.compile('\$\{[^\}]*\}')
 
+def get_daemon_path():
+    return '/home/denarius/denarius/src/denariusd'
+
+def get_linux_user():
+    return os.environ['USER']
+
 def get_config_path():
     return DCONF
 
 def get_config():
     return OrderedDict(parse_config(get_config_path()))
+
+def write_config(config):
+    with open(get_config_path(), "w") as f:
+        for k, v in config.items():
+            line='%s=%s' % (k, v)
+            f.write('%s\n' % line.strip())
+    return True
+
+def write_initd():
+    config = get_mn_config()
+    daemon_name='dnr'
+
+    print (
+    """
+cd /etc/init.d
+    """
+    )
+
+
+    daemon_initd = """
+#! /bin/sh
+# Provides:          {daemon_name}
+# Required-Start:    $remote_fs $syslog
+
+case "$1" in
+  start)
+    echo "Starting daemon"
+    sudo -u {user} {daemon}
+    ;;
+  stop)
+    echo "Stopping daemon"
+    sudo -u {user} {daemon} stop
+    ;;
+  *)
+    echo "Usage: /etc/init.d/{daemon_name} (start|stop)"
+    exit 1
+    ;;
+esac
+
+exit 0
+""".format(daemon_name=daemon_name, daemon=get_daemon_path(), user=get_linux_user())
+
+    masternode_initd = """
+#! /bin/sh
+# Provides:          masternode
+# Required-Start:    $remote_fs $syslog {daemon_name}
+
+case "$1" in
+  start)
+    echo "Starting masternode"
+    sleep 10
+    sudo -u {user} {daemon} masternode start
+    ;;
+  stop)
+    echo "Stopping masternode"
+    sudo -u {user} {daemon} masternode stop
+    ;;
+  *)
+    echo "Usage: /etc/init.d/masternode (start|stop)"
+    exit 1
+    ;;
+esac
+
+exit 0
+""".format(daemon_name=daemon_name, daemon=get_daemon_path(), user=get_linux_user())
+
+    print ('/etc/init.d/%s' % daemon_name)
+    print (daemon_initd)
+
+    print ('/etc/init.d/masternode')
+    print (masternode_initd)
+
+    print (
+    """
+cd /etc/init.d
+chmod 755 masternode
+update-rc.d masternode defaults
+chmod 755 {daemon_name}
+update-rc.d {daemon_name} defaults
+    """.format(daemon_name=daemon_name)
+    )
+
+    return True
 
 def ensure_config_exists():
     conf = get_config_path()
@@ -63,6 +152,33 @@ def get_port():
 
 def get_mn_config_path():
     return MASTERCONF
+
+def get_mn_config():
+    return parse_mn_config(get_mn_config_path())
+
+def write_mn_config(opts):
+    conf=get_config()
+    with open(get_mn_config_path(), "w") as f:
+        f.write('%s %s:%s %s %s %s' % (opts['alias'].strip(), conf['externalip'], conf['port'], conf['masternodeprivkey'], opts['txhash'], opts['txindex']))
+    return True
+
+def parse_mn_config(config_path):
+    config = OrderedDict();
+    with open(config_path) as f:
+        for line in f:
+            line = line.strip()
+            els = line.split()
+            config['alias'] = els[0]
+            config['ipport'] = els[1]
+            config['masternodeprivkey'] = els[2]
+            config['txhash'] = els[3]
+            config['txindex'] = els[4]
+    return config
+
+def ensure_mn_config_exists():
+    conf = get_mn_config_path()
+    if not os.path.isfile(conf):
+        raise Exception("%s is missing" % conf)
 
 def decode_escaped(escaped):
     return __escape_decoder(escaped)[0]
@@ -85,19 +201,6 @@ def parse_config(config_path):
                     v = decode_escaped(v[1:-1])
 
             yield k, v
-
-def write_mn_config(opts):
-    conf=get_config()
-    with open(get_mn_config_path(), "w") as f:
-        f.write('%s %s:%s %s %s %s' % (opts['alias'].strip(), conf['externalip'], conf['port'], conf['masternodeprivkey'], opts['txhash'], opts['txindex']))
-    return True
-
-def write_config(config):
-    with open(get_config_path(), "w") as f:
-        for k, v in config.items():
-            line='%s=%s' % (k, v)
-            f.write('%s\n' % line.strip())
-    return True
 
 def get_random_alias():
     return "%s%s" % (ALIAS, subprocess.check_output(["openssl", "rand", "-hex", "5"]))
@@ -122,3 +225,4 @@ def get_masternode_outputs():
       return json.loads(subprocess.check_output([DAEMON_PATH, "masternode","outputs"]))
     except subprocess.CalledProcessError as e:
       raise Exception( "cannot generate a masternode outputs" )
+
